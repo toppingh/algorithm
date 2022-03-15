0da5b698f08ec7417ae231020d691abf0e0bb448
hekdms <reyna11@naver.com>
2022-03-15 23:50:30 +0900

    기존에 푼 문제 추가

---
+++ 2588번.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int A, B;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    printf("%d\n", A * (B % 10));
+    printf("%d\n", A * ((B / 10) % 10));
+    printf("%d\n", A * (B / 100));
+    printf("%d", A * B);
+
+    return 0;
+}
