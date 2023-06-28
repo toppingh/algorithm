dee333796932536ba1ce8751be00680088441a42
topping <98543258+toppingh@users.noreply.github.com>
2023-06-28 19:23:25 +0900

    Add files via upload

---
+++ 가위바위보.py
@@ -0,0 +1,9 @@
+user = input("가위/바위/보 중 하나를 내세요 : ")
+com = "바위"
+
+if user == "가위":
+    print("당신이 졌습니다!!")
+elif user == " 바위" :
+    print("비겼습니다!!")
+else :
+    print("당신이 이겼습니다!!")
---
+++ 반복문.py
@@ -0,0 +1,10 @@
+for i in range(0, 5):
+    print("Hello World!")
+
+for i in range(0, 5) :
+    print(i)
+
+i = 0
+while i < 5 :
+    print("%d 번째 반복, Hellow world!" % i)
+    i += 1
